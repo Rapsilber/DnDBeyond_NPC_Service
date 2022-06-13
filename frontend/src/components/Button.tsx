04e09dca7ac3f456290474b9e255f2f58143c0d2
Caleb Rapsilber <51001677+Rapsilber@users.noreply.github.com>
2022-06-13 16:13:50 -0500

    Delete frontend directory

--- frontend/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const Button = (props: any) => {
-    return (
-        <button onClick={props.onClick} className={props.className}>{props.children}</button>
-    );
-}
-
-export default Button;
